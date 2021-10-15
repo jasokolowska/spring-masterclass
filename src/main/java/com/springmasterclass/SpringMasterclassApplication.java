1529009f7cb9d069460481fe4c6eac63ebfaf751
Joanna Sokolowska <joanna_s@op.pl>
2021-10-15 20:19:28 +0200

    first commit

---
+++ src/main/java/com/springmasterclass/SpringMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.springmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMasterclassApplication.class, args);
+    }
+
+}
